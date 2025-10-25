7ff521c0a2281f8786c33b5ad229d60d592287a7
mevvvvv <ddasdwasddwaawawwd@gmail.com>
2025-10-24 21:49:19 -0400

    Update emotes.lua

--- emotes.lua
+++ emotes.lua
@@ -1,3 +1,19 @@
 return {
-  ["Needy-Dance"] = 81280454777356
+  ["Needy-Dance"] = 81280454777356,
+  ["zesty-sturdy"] = 120702604995324,
+  ["SHAKE-THAT-THANG"] = 122966616531023,
+  ["watch-it-move"] = 101792282076700,
+  ["Popular"] = 71302743123422,
+  ["Needy-Dance"] = 81280454777356,
+  ["Needy-Dance"] = 81280454777356,
+  ["Party-Rock-Anthem"] = 113548565670128,
+  ["BEST-Dougie-Everybody-Loves-Me"] = 114756515400742,
+  ["The-Tylil-Dance-Kai-Cenat-Dance"] = 107010273569673,
+  ["Jabba-Switchway"] = 103538719480738,
+  ["Confess-to-Me"] = 105722319548703,
+  ["Needy-Dance"] = 81280454777356,
+  ["Confess-to-Me"] = 105722319548703,
+  ["Die-Lit"] = 103285301180445,
+  ["Druski-Pop-Out"] = 98405298116702,
+  ["Rollie"] = 81541548896210
 }
